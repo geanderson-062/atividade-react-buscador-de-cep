0db5827fda2e91e6df71ff417152c00a95ad4794
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-09 13:19:44 -0300

    comentando o código e resolvendo problemas

--- src/App.js
+++ src/App.js
@@ -1,75 +1,57 @@
-import { useState } from 'react'; //cahamndo a api react 
-import { FiSearch } from 'react-icons/fi'; //chamando o iccone de pesquisa
-import './styles.css'; //chamdo o arquivo css desse projeto
+import { useState } from "react"; // Importando o hook useState do React
+import { FiSearch } from "react-icons/fi"; // Importando o ícone de pesquisa
+import "./styles.css"; // Importando o arquivo CSS desse projeto
 
-import api from './services/api';
-
-//aqui manipulamos o html
-
+import api from "./services/api"; // Importando o módulo de API personalizado
 
 function App() {
+  const [input, setInput] = useState(""); // Definindo o estado para o valor do input
+  const [cep, setCep] = useState({}); // Definindo o estado para o objeto do CEP
 
-       const [input, setInput] = useState('');
-       const [cep, setCep] = useState({});
-
-      async function handleSearch(){
- 
-        if(input ===''){
-          alert("preencha algum cep")
-          return;
-        }  
-        
-          try{
-
-           const response = await api.get('${input}/json')//ta dando erro https://www.youtube.com/watch?v=oy4cbqE1_qc a 33:50
-             setCep(response.data)
-             setInput("");//zerando input
-
-          }catch{
- 
-            alert("Ops erro ao buscar");
-            setInput("")
-
-          }
+  async function handleSearch() {
+    if (input === "") {
+      // Verificando se o input está vazio
+      alert("Preencha algum CEP"); // Exibindo um alerta caso esteja vazio
+      return;
     }
 
+    try {
+      const response = await api.get(`${input}/json`); // Fazendo a requisição à API com o valor do input
+      setCep(response.data); // Atualizando o estado do CEP com os dados da resposta da API
+      setInput(""); // Limpando o valor do input após a busca
+    } catch (error) {
+      alert("Ops, erro ao buscar o CEP"); // Exibindo um alerta em caso de erro na requisição
+      setInput(""); // Limpando o valor do input após o erro
+    }
+  }
 
   return (
     <div className="container">
+      <h1 className="title">Buscador CEP</h1>
 
-          <h1 className="title">Buscador Cep</h1>
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
 
-          <div className="containerInput">
-
-            <input type="text" placeholder="Digite seu cep..."
-            
-            value={input} 
-            onChange={(e) => setInput(e.target.value) } 
-            
-            />
-
-            
-             
-            <button className="buttonSearch" onClick={handleSearch}>
-              <FiSearch size={25} color="#fff"/>
-            </button>
-
-          </div>
-
-          {Object.keys(cep).length > 0 &&(
-
-          <main className="main">
-
-            <h2>CEP: {cep.cep}</h2>
-
-               <span>{cep.logradouro}</span>
-               <span>Complemento: {cep.complemento}</span>
-               <span>{cep.bairro}</span>
-               <span>{cep.localidade} - {cep.uf}</span>
-      
-            </main>
-          )}
-
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from "axios"; // Importando o módulo axios
 
-
+// Criando uma instância do axios com a baseURL configurada
 const api = axios.create({
-    baseURL: "https://viacep.com.br/ws/"
+  baseURL: "https://viacep.com.br/ws/",
 });
 
-export default api;
+export default api; // Exportando a instância do axios como um módulo padrão
